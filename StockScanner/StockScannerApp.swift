1055ac2bb03cef06ce0162c53b66b5c9110b62e9
Colstin Donaldson <colstin50@gmail.com>
2023-08-08 16:59:00 -0700

    Initial Commit

---
+++ StockScanner/StockScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockScannerApp.swift
+//  StockScanner
+//
+//  Created by Colstin Donaldson on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct StockScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
