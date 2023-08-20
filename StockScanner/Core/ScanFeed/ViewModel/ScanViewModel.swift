19d97bd5118383eca5208997322061f9ef75f34c
Colstin Donaldson <colstin50@gmail.com>
2023-08-20 15:25:25 -0700

    Add Scan View contentViewModel

--- StockScanner/Core/ScanFeed/ViewModel/ScanViewModel.swift
+++ StockScanner/Core/ScanFeed/ViewModel/ScanViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class ScanViewModel: ObservableObject {
+    @Published var mostActiveStock = [MostActiveStock]()
+    
+    let headers = [
+        "X-RapidAPI-Key": "8961206d8cmsh9d61f46c89a5b1bp175481jsnc56c24646245", 
+        "X-RapidAPI-Host": "mboum-finance.p.rapidapi.com"
+    ]
+    //forHTTPHeaderField = key in the headers dictionary
+    
+    func apiCall() async {
+        // 1. URL
+        if let url = URL(string: "https://mboum-finance.p.rapidapi.com/co/collections/most_actives?start=0"){
+            
+            // 2. headers
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+            
+            // 3. URL Session
+            do {
+                let (data, _) = try await URLSession.shared.data(for: request)
+ 
+                if let dataString = String(data: data, encoding: .utf8){
+                    print(dataString)
+                }
+            } catch {
+                print("Debug api: \(error.localizedDescription)")
+            }
+        }
+    }
+}
